73a1f846deacc65499a695de919e6b05d9cab9dc
unknown <anand.amit943@gmail.com>
2019-06-02 01:03:56 -0700

    table creation and insertion

---
+++ basic.sql
@@ -0,0 +1,10 @@
+-- USE contacts;
+-- Insert into person(First_Name,Last_Name)
+-- VALUES ('Amit','Anand'),
+-- ('Abhishek','Anand'),
+-- ('Abhinay','Anand'),
+-- ('Archana','Anand');
+
+-- Insert into person(First_Name,Middle_Name,Last_Name)
+-- VALUES ('Naveen','Kumar','Sharma'),
+-- ('Shakti','Sharma','Vashisth');
